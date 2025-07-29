f4a14dde28626b9b60acceac3c2bb7522a7e9229
Krystof2so <christo.2so@gmail.com>
2025-07-30 00:32:25 +0200

    [UI] transparent theme nord

--- lua/plugins/nord.lua
+++ lua/plugins/nord.lua
@@ -2,8 +2,14 @@ return {
   {
     "shaunsingh/nord.nvim",
     lazy = false,
-    priority = 1000, -- s'assurer qu'il est chargé en premier
+    priority = 1000, -- chargé avant les autres plugins pour appliquer le thème tôt
     config = function()
+      -- Configuration avant d'activer le thème
+      vim.g.nord_disable_background = true -- rend l'arrière-plan transparent
+      vim.g.nord_disable_float_background = true -- rend les fenêtres flottantes transparentes
+      vim.g.nord_italic = true -- active les italiques (optionnel)
+      vim.g.nord_borders = true -- active les bordures plus visibles (optionnel)
+
       vim.cmd.colorscheme "nord"
     end,
   },
