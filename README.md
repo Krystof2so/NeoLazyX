d34a11c6b45c8e10e7f2285bd2f8db249ee255c6
Krystof2so <christo.2so@gmail.com>
2025-07-29 23:59:09 +0200

    [Doc] update README.md & add comments in lazy.lua

--- README.md
+++ README.md
@@ -56,5 +56,41 @@ Ensuite, lancer la commande `:LazyHealth` pour effectuer un état de santé de l
 
 Les fichiers sous `config/` seront automatiquement chargés au moment opportun, et ce avant vos propres fihciers. 
 
+### *Plugins*
+Pour ajouter un *plugin*, il suffit d'ajouter la spécification du *plugin* à l'un des fichiers sous `lua/plugins/*.lua`. Vous pouvez créer autant de fichiers que vous le souhaitez à cet emplacement.
+Exemple pour le *plugin* **LSP** :
+```lua
+return {
+  -- add symbols-outline
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    opts = {
+      -- add your options that should be passed to the setup() function here
+      position = "right",
+    },
+  },
+}
+```
+
+[Autres exemples de configuration](https://www.lazyvim.org/configuration/examples)
+
+Vous pouvez structurer votre répertoire `lua/plugins` avec un fichier par `plugin` ou un fichier séparé contenant toutes les spécifications des `plugins` pour certaines fonctionnalités.
+
+Par défaut, **LazyVim** installe un certain nombre de *plugins* via ses imports automatiques.
+Mais si vous voulez désactiver un *plugin* spécifique, il suffit d’ajouter un fichier dans le répertoire `lua/plugins/` avec une déclaration spéciale.
+Exemple de désactivation de plusieurs *plugins* :
+```lua
+-- lua/plugins/disabled.lua
+return {
+  { "folke/trouble.nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+}
+```
+
+Cela retourne une table contenant une ou plusieurs déclarations de *plugins*.
+
 ## Commandes spécifiques :
 - `:ShowImage` : Affiche une image dans un terminal externe
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,45 +1,57 @@
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+-- ####################################################################
+-- # FR : Configure lazy.nvim, charge LazyVim et vos propres plugins, #
+-- # puis automatise la mise à jour de Mason au démarrage             #
+-- # ---------------------------------------------------------------- #
+-- # EN : Configure lazy.nvim, load LazyVim and your own plugins,     #
+-- # then automate Mason updates at startup                           #
+-- ####################################################################
+
+-- Clonage de lazy.nvim (branche stable) si non présent :
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim" -- Chemin d'installation local
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
+  if vim.v.shell_error ~= 0 then -- Si échec clonage
     vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { "Echec du clonage de lazy.nvim:\n", "ErrorMsg" },
       { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
+      { "\nPresser une touche pour quitter..." },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
   end
 end
+-- Ajoute lazy.nvim au runtimepath, pour pouvoir le require :
 vim.opt.rtp:prepend(lazypath)
 
+-- Configuration de Lazy et des plugins :
 require("lazy").setup {
   spec = {
-    -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
-    { import = "plugins" },
-    { import = "plugins.mason" },
-    { import = "plugins.nord" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" }, -- charge les plugins de base de LazyVim
+    { import = "plugins" }, -- charge les plugins personnalisés (répertoire local `lua/plugins`)
+    { import = "plugins.mason" }, -- import séparé pour la config de mason
+    { import = "plugins.nord" }, -- import du thème Nord
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    -- Par défaut, seuls les plugins LazyVim seront chargés de manière différée.
+    -- Vos plugins personnalisés seront chargés au démarrage.
+    -- Si vous savez ce que vous faites, vous pouvez définir cette option sur `true`
+    -- pour que tous vos plugins personnalisés soient chargés de manière différée par défaut.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
+    -- Il est recommandé de laisser version=false pour l'instant,
+    -- car de nombreux plugins qui prennent en charge le versionnage
+    -- ont des versions obsolètes, ce qui pourrait endommager votre installation de Neovim.
+    version = false, -- toujours utiliser la dernière version git commit
+    -- version = "*", -- essayez d'installer la dernière version stable pour les plugins qui prennent en charge semver
   },
-  install = { colorscheme = { "nord" } },
+  install = { colorscheme = { "nord" } }, -- Installe le thème Nord juste après l'installation
   checker = {
-    enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
-  }, -- automatically check for plugin updates
+    enabled = true, -- Vérifiez régulièrement les mises à jour des plugins.
+    notify = true, -- Notifier lors de la mise à jour
+  }, -- vérifier automatiquement les mises à jour des plugins
   performance = {
     rtp = {
-      -- disable some rtp plugins
+      -- Désactive certains plugins Vim par défaut inutiles ou obsolètes, pour accélérer le démarrage.
       disabled_plugins = {
         "gzip",
         -- "matchit",
