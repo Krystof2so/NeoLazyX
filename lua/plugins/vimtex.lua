3c2a62835f402d5c6043b403e67c1bf04ca17cdd
Krystof2so <christo.2so@gmail.com>
2025-08-20 00:07:48 +0200

    [plugin] add vimtex
    with simple configuration

---
+++ lua/plugins/vimtex.lua
@@ -0,0 +1,21 @@
+-- ########################################################
+-- # Vimtex                                               #
+-- #                                                      #
+-- # VimTeX est une extension moderne pour Vim et Neovim, #
+-- # destinée à la gestion du type de fichier             #
+-- # et de la syntaxe des fichiers LaTeX.                 #
+-- #                                                      #
+-- # https://github.com/lervag/vimtex                     #
+-- ########################################################
+
+return {
+  {
+    "lervag/vimtex",
+    ft = { "tex", "plaintex" }, -- charge uniquement pour les fichiers LaTeX
+    init = function() -- Fonction d’initialisation exécutée avant que le plugin ne soit chargé
+      vim.g.vimtex_view_method = "general" -- Pour utiliser un viewer pdf personnalisé
+      vim.g.vimtex_view_general_viewer = "evince" -- Définit la commande du viewer
+      vim.g.vimtex_view_general_options = "@pdf" -- 'placeholder' -> '@pdf' remplacé par le nom du fichier .pdf à ouvrir
+    end,
+  },
+}
